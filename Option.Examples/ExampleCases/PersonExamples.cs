7b8d75524be1956eb4546016ddb7c5f7eb1ee0d2
xcgpseud <chris.evans92@hotmail.com>
2022-08-09 19:59:09 +0100

    Add an example

---
+++ Option.Examples/ExampleCases/PersonExamples.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Option.Examples.ExampleCases;
+
+[TestFixture]
+public class PersonExamples
+{
+    [Test]
+    public void GetPerson_LogPersonNameIfValid_ThrowIfInvalid()
+    {
+        var repository = FakeRepository.Make();
+
+        var person = Option<Person>
+            .From(repository.Get(1))
+            .IfValid(person => Console.WriteLine($"Person name: {person.FirstName} {person.LastName}"))
+            .GetOrThrow(new Exception());
+
+        person.Should().BeOfType<Person>();
+    }
+}
---
+++ Option.Examples/FakeRepository.cs
@@ -0,0 +1,28 @@
+﻿namespace Option.Examples;
+
+public class FakeRepository
+{
+    private readonly List<Person> _people;
+
+    public FakeRepository()
+    {
+        _people = Enumerable.Range(1, 5)
+            .Select(i => new Person
+            {
+                Id = i,
+                FirstName = Faker.Name.First(),
+                LastName = Faker.Name.Last(),
+            })
+            .ToList();
+    }
+
+    public static FakeRepository Make()
+    {
+        return new FakeRepository();
+    }
+
+    public Person? Get(int id)
+    {
+        return _people.FirstOrDefault(person => person.Id == id);
+    }
+}
