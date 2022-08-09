7b85cc872cec1721906d3efee44bdc23855b99f9
xcgpseud <chris.evans92@hotmail.com>
2022-08-09 19:23:21 +0100

    Add final files for improved test structure and remove leftover random letter

---
+++ Option.Tests/TestBase.cs
@@ -0,0 +1,20 @@
+﻿using Option.Interfaces;
+
+namespace Option.Tests;
+
+public class TestBase
+{
+    protected const string ValidValue = TestConstants.Values.Valid;
+    protected const string DefaultValue = TestConstants.Values.Default;
+    protected const string ExceptionMessage = TestConstants.ExceptionMessage;
+    
+    protected static IOption<string> GetSome()
+    {
+        return Option<string>.From(ValidValue);
+    }
+
+    protected static IOption<string> GetNone()
+    {
+        return Option<string>.From(null);
+    }
+}
---
+++ Option.Tests/TestConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Option.Tests;
+
+public static class TestConstants
+{
+    public static class Values
+    {
+        public const string Valid = "Valid Value";
+        public const string Default = "Default Value";
+    }
+
+    public const string ExceptionMessage = "Exception Message";
+}
